9b9ccd27ab3579843306ae585ef2234fbcbb057b
Gordon D <gordon.draine97@yahoo.com>
2024-01-17 18:46:35 -0500

    adding main folder contents

--- responses.py
+++ responses.py
@@ -13,3 +13,8 @@ def get_response(message: str) -> str:
         return '`This is a help message that you can modify.`'
     
         return 'I didnt understand what you wrote, try typing "!help".'
+    
+    # //////////////////////////////////////////////////////////////////////////////////
+
+    def get_response(user_input: str) -> str: 
+        raise NotImplementedError('missing code...')
