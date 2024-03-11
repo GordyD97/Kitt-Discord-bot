fab8d472553987f55da8cda7cb05c9593e613702
Gordon D <gordon.draine97@yahoo.com>
2024-03-10 23:46:51 -0400

    adding new bot file

--- bot.py
+++ bot.py
@@ -1,11 +1,30 @@
 import discord
 import responses
+import random
+
+# Define a list of mean comments
+mean_comments = [
+    "You're not very bright, are you?",
+    "You couldn't find your way out of a paper bag.",
+    "Do you ever listen to yourself?",
+    "I've met smarter sandwiches.",
+    "Why don't you try thinking before you speak?",
+    "Did you even read the instructions?",
+    "Your brain must be the size of a pea.",
+    "You're about as useful as a screen door on a submarine."
+]
 
 
 async def send_message(message, user_message, is_private):
     try:
+        # Check if the user's message is a question
+        if user_message.startswith('?'):
+            response = responses.get_response(user_message[1:])
+        else:
+            # Select a random mean comment
+            response = random.choice(mean_comments)
 
-        response = responses.get_response(user_message)
+        # Send the response to the user
         await message.author.send(response) if is_private else await message.channel.send(response)
 
     except Exception as e:
@@ -13,10 +32,11 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 
-    intetns = discord.Intents.default()
+    # Replace TOKEN with your actual Discord bot token
+    TOKEN = "YOUR_DISCORD_BOT_TOKEN_HERE"
+    intents = discord.Intents.default()
     intents.message_content = True
-    client = discord.Client(intents=intetns)
+    client = discord.Client(intents=intents)
 
     @client.event
     async def on_ready():
@@ -27,16 +47,22 @@ def run_discord_bot():
         if message.author == client.user:
             return
 
-    username = str(message.author)
-    user_message = str(message.content)
-    channel = str(message.channel)
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
 
-    print(f'{username} said: "{user_message}" ({channel})')
+        print(f'{username} said: "{user_message}" ({channel})')
 
-    if user_message[0] == '?':
-        user_message = user_message[1:]
-        await send_message(message, user_message, is_private=True)
-    else:
-        await send_message(message, user_message, is_private=False)
+        # Check if the user's message is a question
+        if user_message.startswith('?'):
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
 
     client.run(TOKEN)
+
+
+# Run the bot
+if __name__ == '__main__':
+    run_discord_bot()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Final
-import os 
+import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import get_response
@@ -8,24 +8,54 @@ load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
 
-# initial bot set up 
+# initial bot set up
 
 intents: Intents = Intents.default()
-intents.message_content = True 
+intents.message_content = True
 client: Client = Client(intents=intents)
 
 # message func
 
-async def send_message(message: Message, user_message: str) -> None: 
+
+async def send_message(message: Message, user_message: str) -> None:
     if not user_message:
         print('(Message was empty becasue intents were not enabled probaly...darn)')
-    return
+        return
 
     if is_private := user_message[0] == '?':
         user_message = user_message[1:]
 
-    try: 
+    try:
         response: str = get_response(user_message)
         await message.author.send(response) if is_private else message.channel.send(response)
-    except Exception as e: 
+    except Exception as e:
         print(e)
+
+        #  bot start up /////////////////////////////////////////////////////
+
+    @client.event
+    async def on_ready() -> None:
+        print(f'{client.user} is now running!')
+
+    # handling incoming messages
+
+    async def on_message(message: Message) -> None:
+        if message.author == client.user:
+            return
+
+        username: str = str(message.author)
+        user_message: str = message.content
+        channel: str = str(message.channel)
+
+        print(f'[{channel}] {username}:"{user_message}"')
+        await send_message(message, user_message)
+
+        # main entry point
+
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+
+if __name__ == '__main__':
+    main()
